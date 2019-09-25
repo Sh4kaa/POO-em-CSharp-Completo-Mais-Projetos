d81335fb465c37b4a85884b44832c5d646b7a95e
Sh4Kaa <darioo_dario@hotmail.com>
2019-09-25 12:53:56 -0300

    Exercio de Fixação
    exercicio

---
+++ Recapitulacao_logica_de_Programacao/exerciciofixacao/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerciciofixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine("{0}, cujo preço é $ {1:f2}", produto1, preco1);
+            Console.WriteLine("{0} cujo preço é {1:f2}", produto2, preco2);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Medida com oitro casas decimais: {0:f8}", medida);
+            Console.WriteLine("Arredondamento (três casas decimais): {0:f3}", medida);
+            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("f3", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+        }
+    }
+}
