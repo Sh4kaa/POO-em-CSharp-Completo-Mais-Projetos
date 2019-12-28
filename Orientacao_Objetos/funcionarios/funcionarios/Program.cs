d4cb1ec89b798e537f9231204564622e1152ebdc
Dario <darioo_dario@hotmail.com>
2019-12-28 16:21:02 -0300

    exercicio2
    Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário médio dos funcionários.

---
+++ Orientacao_Objetos/funcionarios/funcionarios/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+namespace funcionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario(); // chamei classe, criei uma variavel e instanciei a classe;
+            Funcionario f2 = new Funcionario();
+
+            //dados primeiro funcionário
+            Console.WriteLine("Dados do primeiro funcionário");
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.Salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            //dados segundo funcionario
+
+            Console.WriteLine("Dados do segundo funcionário");
+            Console.Write("Nome: ");
+
+            f2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (f1.Salario + f2.Salario) / 2;
+
+
+            Console.WriteLine("Salário médio = "+ media.ToString("f2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
