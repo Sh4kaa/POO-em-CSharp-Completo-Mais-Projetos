8a5b1e3ecebd2d630d3ef6dbd040bccb4fea521d
Dario <darioo_dario@hotmail.com>
2019-12-28 16:00:15 -0300

    Exercicio Pessoas
    Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.

---
+++ Orientacao_Objetos/exercicio1/exercicio1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoas p1 = new Pessoas(); // estanciando o objeto da classe Pessoas
+            Pessoas p2 = new Pessoas();
+
+
+            // dados da primeira pessoa, nome e idade
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine(); // chamando e lendo o objeto nome da classe pessoas
+            Console.Write("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            // dados da segunda pessoa
+
+            Console.WriteLine("Dados da segunda pessoa;");
+            Console.Write("Nome: ");
+
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if(p2.Idade > p1.Idade)
+            {
+                Console.WriteLine("A pessoa mais velha é " + p2.Nome);
+            } else
+            {
+                Console.WriteLine("A pessoa mais velha é " + p1.Nome);
+            }
+
+
+            
+
+
+
+        }
+    }
+}
