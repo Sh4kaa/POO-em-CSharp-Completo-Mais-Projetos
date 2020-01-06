960bc0fbb32128a9c3902474f66fbef7d69dfa61
Dario <darioo_dario@hotmail.com>
2020-01-06 13:52:08 -0300

    completo tringulo -ignore

--- Orientacao_Objetos/Classes_Atributos_Metodos_MembrosEstaticos/exercicio1/Triangulo.cs
+++ Orientacao_Objetos/Classes_Atributos_Metodos_MembrosEstaticos/exercicio1/Triangulo.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace exercicio1
 {
     class Triangulo
@@ -6,5 +7,13 @@ namespace exercicio1
         public double A;
         public double B;
         public double C;
+
+        //método (Função dentro da própria classe)
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return raiz;
+        }
     }
 }
