948bcd473ff71b0f771f104a86e1127f56d1dc03
Dario <darioo_dario@hotmail.com>
2020-01-06 21:52:18 -0300

    Exercicio Produto (aula 43 - Objecto ToString)
    Resolvendo um problema com objetos

---
+++ Orientacao_Objetos/42_ComecResolverSegExemplo/produtos/produtos/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace produtos
+{
+    class Produto
+    {
+        // atributos
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("f2", CultureInfo.InvariantCulture)
+                + ", " + Quantidade + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("f2", CultureInfo.InvariantCulture);
+        }
+
+
+
+    }
+}
