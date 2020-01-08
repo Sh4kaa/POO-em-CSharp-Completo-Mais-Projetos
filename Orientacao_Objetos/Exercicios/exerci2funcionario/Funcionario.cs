87bb21adfbc2f7fcdbc42dc95a32e93122fd919c
Sh4Kaa <darioo_dario@hotmail.com>
2020-01-08 14:31:11 -0300

    exercicio 2 salario de um funcionario

---
+++ Orientacao_Objetos/Exercicios/exerci2funcionario/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+namespace exerci2funcionario
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        // metódos
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto +=  SalarioBruto * porcentagem / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Nome +" R$ " + SalarioLiquido().ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Orientacao_Objetos/Exercicios/exerci2funcionario/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace exerci2funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+
+            Console.Write("Salário bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Funcionário: " + func);
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double perc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(perc);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + func);
+
+
+            
+            
+        }
+    }
+}
