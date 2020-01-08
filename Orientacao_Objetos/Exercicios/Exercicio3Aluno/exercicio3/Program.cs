90d6d8ee40598fca6f14686de2f203ab962ee1a0
Sh4Kaa <darioo_dario@hotmail.com>
2020-01-08 17:31:07 -0300

    exercicio3 Notas do aluno

---
+++ Orientacao_Objetos/Exercicios/Exercicio3Aluno/exercicio3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno al = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            al.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            al.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            al.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            al.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);          
+                       
+            Console.WriteLine("NOTA FINAL = " + al.NotaFinal().ToString("f2", CultureInfo.InvariantCulture));
+
+            if (al.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + al.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)
+                + " PONTOS");
+
+            }
+        }
+    }
+}
