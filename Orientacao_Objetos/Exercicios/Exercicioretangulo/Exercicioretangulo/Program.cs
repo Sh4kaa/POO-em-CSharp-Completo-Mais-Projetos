1450c9577396e226ca8ab1cf20ec15e3b85987a6
Dario <darioo_dario@hotmail.com>
2020-01-07 21:28:30 -0300

    exerccio Retangulo
    nesse exercicio foi abordado o calculo
    da AREA, PERIMETRO E DIAGONAL de um retangulo

---
+++ Orientacao_Objetos/Exercicios/Exercicioretangulo/Exercicioretangulo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicioretangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+            
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = "+ ret.Area().ToString("f2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("PERÍMETRO = " + ret.Perimetro().ToString("f2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("f2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
