8da1b09abac4254e7cf4bb75be2251223a9d855a
Sh4Kaa <darioo_dario@hotmail.com>
2020-01-13 12:38:12 -0300

    Cotação Dolar
    exemplo de classe estatica

---
+++ Orientacao_Objetos/Classes_Atributos_Metodos_MembrosEstaticos/MembrosEstaticos/conversao/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace conversao
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+        public static double CotacaoDolar(double cot, double quantDol)
+        {
+            double total = cot * quantDol;
+            return total + total * Iof / 100.0;         
+
+        }
+
+        
+    }
+}
---
+++ Orientacao_Objetos/Classes_Atributos_Metodos_MembrosEstaticos/MembrosEstaticos/conversao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace conversao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é  a cotação do dolar: ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar: ");
+            double quantDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.CotacaoDolar(cotacao, quantDolar);
+
+
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("f2", CultureInfo.InvariantCulture));
+        }
+    }
+}
