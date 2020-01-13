310b14e4b90f12877ce8d91c3706896a71d447a4
Sh4Kaa <darioo_dario@hotmail.com>
2020-01-13 11:10:51 -0300

    Membros estaticos parte2

---
+++ Orientacao_Objetos/Classes_Atributos_Metodos_MembrosEstaticos/MembrosEstaticos/exercicioraio/exercicioraio/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercicioraio
+{
+    class Calculadora
+    {
+       public double Pi = 3.14;
+
+        public double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r; // formula do raio
+        }
+
+        public double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0); // 4 terços de * Pi * raio elevado ao cubo. formula do volume
+        }
+    }
+}
--- Orientacao_Objetos/Classes_Atributos_Metodos_MembrosEstaticos/MembrosEstaticos/exercicioraio/exercicioraio/Program.cs
+++ Orientacao_Objetos/Classes_Atributos_Metodos_MembrosEstaticos/MembrosEstaticos/exercicioraio/exercicioraio/Program.cs
@@ -6,29 +6,23 @@ namespace exercicioraio
 {
     class Program
     {
-        static double Pi = 3.14;
+        
         static void Main(string[] args)
         {
+            Calculadora calc = new Calculadora();
+
             Console.Write("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circunferencia(raio);
+            double circ = calc.Circunferencia(raio);
             Console.WriteLine("Circunferência: " + circ.ToString("f2", CultureInfo.InvariantCulture));
 
-            double volume = Volume(raio);
+            double volume = calc.Volume(raio);
             Console.WriteLine("Volume: " + volume.ToString("f2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: "+ Pi.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: "+ calc.Pi.ToString("f2", CultureInfo.InvariantCulture));
             
         }
 
-        static double Circunferencia(double r)
-        {
-            return 2.0 * Pi * r; // formula do raio
-        }
-
-        static double Volume(double r)
-        {
-             return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0); // 4 terços de * Pi * raio elevado ao cubo. formula do volume
-        }
+        
     }
 }
