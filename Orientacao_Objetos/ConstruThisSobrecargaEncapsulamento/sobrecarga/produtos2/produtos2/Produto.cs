07fac96be078052dd11ac98dd5149ac5783332bf
Dario <darioo_dario@hotmail.com>
2020-01-15 21:50:17 -0300

    SOBRECARGA
    sobrecarga

---
+++ Orientacao_Objetos/ConstruThisSobrecargaEncapsulamento/sobrecarga/produtos2/produtos2/Produto.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace produtos2
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //construtor 1
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //construtor 2 SOBRECARGA
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 5; // ainda assim não é necessario adicionar valores se no caso quiser que seja zero, só naõ adicionar o atributo ao construtor
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade) // FUNÇÃO COOM VOID
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", " + Quantidade + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        /* QUANDO OS PARAMETROS DA FUNÇÃO JÁ ESTÃO NA CLASSE, É NECESSARIO TER UM RETURN
+        NO CASO UAM FUNÇÃO VAZIA (VOID), EM QUE OS PARAMETROS ESTÃO EM OUTRO LUGAR E VAMOS PEGA-LOS
+        A FUNÇÃO TEM QUE TER VOID*/
+    }
+}
