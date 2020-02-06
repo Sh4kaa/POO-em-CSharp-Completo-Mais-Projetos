36de2d2edbaa17aaa2a728ee1f5ac224af67f69c
Dario <darioo_dario@hotmail.com>
2020-02-06 00:41:14 -0300

    upcont

---
+++ Orientacao_Objetos/Exercicios/Conta/ContaBanco/ContaBancaria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBanco
+{
+    class ContaBancaria
+    {
+        public int Numero;
+        public string Titular;
+        public double Saldo;
+
+        
+        
+        // construtor
+        public ContaBancaria(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+    }
+}
---
+++ Orientacao_Objetos/Exercicios/Conta/ContaBanco/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta = new ContaBancaria(0711-3, "Dario", 500.00);
+
+            Console.WriteLine(conta.Titular);
+        }
+    }
+}
