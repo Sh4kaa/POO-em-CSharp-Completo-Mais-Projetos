99b0e71636d56bd0d7d7558e33b390461ab59908
Sh4Kaa <darioo_dario@hotmail.com>
2020-02-20 16:49:47 -0300

    VETORES PART1
    um exercicio simples apenas para relembrar vetores onde se calcula a média da altura de N pessoas

---
+++ Orientacao_Objetos/Vetores/vetores/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double soma = 0.0;            
+            for (int i = 0; i < n; i++)
+            {
+                soma = soma + vet[i];                
+            }
+
+            double media = soma / n;
+            Console.WriteLine("AVERAGE HEIGHT = "+ media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
