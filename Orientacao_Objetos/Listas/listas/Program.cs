11a53f328b3c893dec84a8fe9133d57e4936766b
Sh4Kaa <darioo_dario@hotmail.com>
2020-03-03 18:13:24 -0300

    exercicios Listas Final

--- Orientacao_Objetos/Listas/listas/Program.cs
+++ Orientacao_Objetos/Listas/listas/Program.cs
@@ -12,9 +12,11 @@ namespace listas
 
             lista.Add("Maria");
             lista.Add("Alex");
-            lista.Add("joazinho");
-            lista.Add("Anazinha");
-            lista.Insert(2, "Bob"); // inserir elementos na lista em uma posição especifica
+            lista.Add("Bob");
+            lista.Add("Anna");
+            lista.Insert(2, "Marco"); // inserir elementos na lista em uma posição especifica
+
+            //predicado tema  ver com função e envolve expressão lambda
 
             foreach(string obj in lista) // varrer a lista
             {
@@ -28,10 +30,51 @@ namespace listas
 
             Console.WriteLine("First 'A': "+ s1);
 
-            string s2 = lista.FindLast(y => y[0] == 'A'); // busca ultima ocorrencia no vetor que comece com 'A'
+            string s2 = lista.FindLast(y => y[0] == 'A'); // busca ultima ocorrencia na lista que comece com 'A'
 
             Console.WriteLine("Last 'A': "+ s2);
+
+            int pos1 = lista.FindIndex(x => x[0] =='A'); // encontra a primeira posição do elemento da lista que contenha 'A (com base nas posições)
+
+            Console.WriteLine("Fisrt position 'A': "+ pos1);
+
+            int pos2 = lista.FindLastIndex(x => x[0] == 'A'); // encontra a ultima posição do elemento da lista que contenha o caractere 'A' (com base nas posiçõees)
+
+            Console.WriteLine("Fisrt position 'A': " + pos2);
+
+
+            List<string> list2 = lista.FindAll(x => x.Length == 5); /* gera uma nova lista com base na primeira, trazendo os
+                                                                     elementos que contenham 5 caracteres*/
+                      
+            foreach(string obj2 in list2)
+            {
+                Console.WriteLine(obj2);
+            }
+
+            Console.WriteLine("--------------");
+
+            lista.RemoveAt(3); // remove uma ocorrencia da posicao 3 da lista
+            //lista.Remove("Alex"); remove apenas o alex
+            // lista.RemoveRange(2, 2); remove a partir da posição 2 e remove mais 2 ocorrencias;
+
+
+            foreach (string obj2 in lista)
+            {
+                Console.WriteLine(obj2);
+            }
+
+            Console.WriteLine("----------");
+
+            lista.RemoveAll(x => x[0] == 'M'); // remove todas as ocorrencias que se iniciaim com 'M' da lista
+
+
+            foreach (string obj2 in lista)
+            {
+                Console.WriteLine(obj2);
+            }
+                     
             
+
         }
         static bool Test(string s)
         {
