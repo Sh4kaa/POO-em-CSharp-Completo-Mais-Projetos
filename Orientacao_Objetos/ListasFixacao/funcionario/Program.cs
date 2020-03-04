c9f3fafacbffc1056b965ce7b01f44368463f527
Sh4Kaa <darioo_dario@hotmail.com>
2020-03-04 18:16:12 -0300

    exercicio salario
    em andamento

---
+++ Orientacao_Objetos/ListasFixacao/funcionario/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Emplyoee #" + (i + 1)+":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                list.Add(new Employee (id, name, salary));
+
+                Console.WriteLine();
+                
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int searchID = int.Parse(Console.ReadLine());
+
+
+
+
+            Console.WriteLine("Updated list of employees:");
+            Console.WriteLine(list);
+
+        }
+    }
+}
