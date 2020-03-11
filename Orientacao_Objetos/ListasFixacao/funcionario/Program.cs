0896180e7809f463c29aba2488ec24dc7653f39c
Sh4Kaa <darioo_dario@hotmail.com>
2020-03-11 14:28:14 -0300

    Employee
    Funcionario

--- Orientacao_Objetos/ListasFixacao/funcionario/Program.cs
+++ Orientacao_Objetos/ListasFixacao/funcionario/Program.cs
@@ -16,13 +16,14 @@ namespace funcionario
             Console.Write("How many employees will be registered? ");
             int n = int.Parse(Console.ReadLine());
 
-            List<Employee> list = new List<Employee>();
+            List<Employee> list = new List<Employee>(); // estanciando lista do tipo Employee(tipo classe)
 
-            Console.WriteLine();
+            Console.WriteLine(); // pula linha
 
-            for (int i = 0; i < n; i++)
+            // varrendo a lista estanciada
+            for (int i = 1; i <= n; i++)  
             {
-                Console.WriteLine("Emplyoee #" + (i + 1)+":");
+                Console.WriteLine("Emplyoee #" + i +":");
                 Console.Write("Id: ");
                 int id = int.Parse(Console.ReadLine());
                 Console.Write("Name: ");
@@ -30,7 +31,7 @@ namespace funcionario
                 Console.Write("Salary: ");
                 double salary = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
-                list.Add(new Employee (id, name, salary));
+                list.Add(new Employee (id, name, salary)); //passando os parametros e adicionando na lista
 
                 Console.WriteLine();
                 
@@ -39,11 +40,35 @@ namespace funcionario
             Console.Write("Enter the employee id that will have salary increase: ");
             int searchID = int.Parse(Console.ReadLine());
 
+            Employee emp = list.Find(x => x.Id == searchID); /* fazendo a comparação se o Id(objeto) é igual ao id
+            da variavel searchID ("emp" recebe o resultado da comparacao ) */
 
+            if(emp != null) // se oq estiver na lista for diferente de nulo execute abaixo
+            {
+                Console.Write("Enter the percentage: ");
+                double percent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(percent);
+                
+            } else
+            {
+                Console.WriteLine("This id does not exist!");
+                
+            }
 
-
+            Console.WriteLine();
             Console.WriteLine("Updated list of employees:");
-            Console.WriteLine(list);
+            
+            // varrendo a lista e mostrando os valores
+            foreach(Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+
+
+
+
 
         }
     }
