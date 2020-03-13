7b1b99d6b9250fabde222ce042e5f992e3e5896a
Sh4Kaa <darioo_dario@hotmail.com>
2020-03-13 18:18:15 -0300

    Matriz2

---
+++ Orientacao_Objetos/matexercicio/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matexercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n;
+            string[] s = Console.ReadLine().Split(' ');
+            Console.WriteLine("------------");
+
+            m = int.Parse(s[0]);
+            n = int.Parse(s[1]);
+
+            int[,] mat = new int[m, n];
+
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] numbers = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(numbers[j]);
+                }
+            }
+            Console.WriteLine();
+
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (num == mat[i, j])
+                    {
+                        Console.Write("Position " + i + "," + j + ";");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+            
+
+
+        }
+    }
+}
