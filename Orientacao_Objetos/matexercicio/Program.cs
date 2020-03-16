f69e52d042e4dbbe4e5739f51f736165f02fb03b
Dario <darioo_dario@hotmail.com>
2020-03-16 20:08:48 -0300

    matriz de posições resolvida
    Fazer um programa para ler dois números inteiros M e N, e depois ler uma matriz de M linhas por N colunas contendo números inteiros, podendo haver repetições. Em seguida, ler um número inteiro X que pertence à matriz. Para cada ocorrência de X, mostrar os valores à esquerda, acima, à direita e abaixo de X, quando houver, conforme exemplo.

--- Orientacao_Objetos/matexercicio/Program.cs
+++ Orientacao_Objetos/matexercicio/Program.cs
@@ -39,15 +39,39 @@ namespace matexercicio
                 {
                     if (num == mat[i, j])
                     {
-                        Console.Write("Position " + i + "," + j + ";");
+                        Console.WriteLine("Position " + i + "," + j + ";");
+                        if (j > 0) // para imprimir o lado esquerdo a coluna tem que ser maior que 0(considerando que o numero selecionado seja no 0,0)
+                        {
+
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+
+                        }
+                        if (i > 0) // para imprimir o valor acima do numero da matriz selecionado considerando ele estando no zero a linah deve ser maior que 0
+                        {
+
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+
+                        }
+                        if (j < n - 1) // só imprime se a coluna "j" for menor que a ultima coluna - 1
+                        {
+
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+
+                        }
+                        if (i < m - 1) // só imprime se a linha for menor que a ultima linha - 1(supondo que esteja na ultima linha)
+                        {
+
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+
                     }
+                    
+
                 }
-                Console.WriteLine();
+
+
 
             }
-            
-
-
         }
     }
 }
