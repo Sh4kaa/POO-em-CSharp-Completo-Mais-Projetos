5585a55a2d99a28954cf723d0e920dcf7815158d
Dario <darioo_dario@hotmail.com>
2020-03-22 11:38:07 -0300

    Conjuntos+Materiald e apoio
    exemplos de conjuntos juntamente com os pdf's do material de apoio

---
+++ Orientacao_Objetos/conjuntos/conjuntos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conjuntos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+
+            A.Add(3);
+            A.Add(5);
+            A.Add(8);
+            A.Add(9);
+
+            B.Add(3);
+            B.Add(4);
+            B.Add(5);
+
+            /*A.ExceptWith(B);  ver diferença entre conjunto A e B, removendo do conjunto A o que pertence ao B*/
+
+            /*B.Remove(4);*/ //remover
+
+            /*A.UnionWith(B); vai uni conjunto A com B, tirando os repetidos*/
+
+            A.IntersectWith(B);
+
+            foreach(int x in A)
+            {
+                Console.WriteLine(x);
+            }
+
+            /*Console.Write("Digite um numero inteiro: ");
+            int N = int.Parse(Console.ReadLine());
+
+            if (B.Contains(N))
+            {
+                Console.WriteLine(N + " pertence ao conjunto B");
+            }
+            else
+            {
+                Console.WriteLine(N+ " não pertence ao conjunto B");
+            }*/
+
+        }
+    }
+}
