35d22f92ce471762f825d09e46225da59595e624
Dario <darioo_dario@hotmail.com>
2020-03-22 13:09:39 -0300

    exercicios Conjuntos

--- Orientacao_Objetos/exercices conjunto/exercices conjunto/Program.cs
+++ Orientacao_Objetos/exercices conjunto/exercices conjunto/Program.cs
@@ -10,6 +10,61 @@ namespace exercices_conjunto
     {
         static void Main(string[] args)
         {
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+
+            // conjunto A
+            Console.Write("O curso A possui quantos alunos? ");
+            int QuantA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso A: ");
+            
+            for(int i = 0; i < QuantA; i++)
+            {
+                A.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine();
+
+            //conjunto B
+            Console.Write("O curso B possui quantos alunos? ");
+            int QuantB = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso B: ");
+
+            for (int i = 0; i < QuantB; i++)
+            {
+                B.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine();
+
+            //conjunto C
+            Console.Write("O curso C possui quantos alunos? ");
+            int QuantC = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso C: ");
+
+            for (int i = 0; i < QuantC; i++)
+            {
+                C.Add(int.Parse(Console.ReadLine()));
+            }
+
+            // unindo os conuntos A com B e C
+            A.UnionWith(B);
+            A.UnionWith(C);
+
+            //varrendo o conjunto
+            int cont = 0;
+            foreach(int x in A)
+            {
+                cont++;
+            }
+
+            // saida dos dados
+            Console.Write("Total de alunos: "+ cont);
+            Console.WriteLine();
+
+
+
+
+
         }
     }
 }
