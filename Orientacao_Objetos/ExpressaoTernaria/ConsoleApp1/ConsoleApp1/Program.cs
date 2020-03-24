57ca4461afea789dac338099ccfb72b9b93cce38
Dario <darioo_dario@hotmail.com>
2020-03-24 17:19:41 -0300

    Expressão ternaria
    resolvendo uma questão com expressões ternarias

---
+++ Orientacao_Objetos/ExpressaoTernaria/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05; // resolvido com expressão ternaria
+            
+            /*
+            if (preco < 20.0)
+            {
+                desconto = preco * 0.1;
+            }
+            else
+            {
+                desconto = preco * 0.05;
+            }
+            modo if e else*
+            método if e esle para resolver a questão*/
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
