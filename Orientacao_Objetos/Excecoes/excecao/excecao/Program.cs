737376a0755d3b98bb489a9ca4c5e2afe3b15fe8
Dario <darioo_dario@hotmail.com>
2020-06-20 12:55:44 -0300

    try catch

---
+++ Orientacao_Objetos/Excecoes/excecao/excecao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excecao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try // bloco try
+            {
+                
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException) // bloco catch
+            {
+                Console.WriteLine("Division by zero is not allowed");
+            }
+            catch (FormatException e) // bloco catch 2
+            {
+                Console.WriteLine("Format error! " + e.Message);
+            }
+        }
+    }
+}
