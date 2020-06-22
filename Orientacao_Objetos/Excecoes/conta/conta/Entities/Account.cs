45e7fc265c4b4f6e2ca5ea22d2c6e9b8e2555c17
Dario <darioo_dario@hotmail.com>
2020-06-22 17:05:06 -0300

    Exercicio de Try Catch

---
+++ Orientacao_Objetos/Excecoes/conta/conta/Entities/Account.cs
@@ -0,0 +1,40 @@
+﻿using conta.Entities.Exceptions;
+
+namespace conta.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double WithDrawLimit { get; set; }
+        public double Balance { get; set; }
+
+        public Account(int number, string holder, double withDrawLimit, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            WithDrawLimit = withDrawLimit;
+            Balance = balance;
+        }
+
+        public void  Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > WithDrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("Not enough balance");
+            }
+
+            Balance -= amount;
+        }
+
+    }
+}
