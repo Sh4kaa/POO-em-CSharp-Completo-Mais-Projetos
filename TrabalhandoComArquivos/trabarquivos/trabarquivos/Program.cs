dfcbeb45cbe46b9a8fd461b479a00c7668e892dc
Sh4kaa <darioo_dario@hotmail.com>
2020-06-25 16:37:57 -0300

    File, FileInfo e IOException

---
+++ TrabalhandoComArquivos/trabarquivos/trabarquivos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+
+namespace trabarquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath); // necessario estanciar
+                fileInfo.CopyTo(targetPath); // copiando para o destino o arquivo
+
+                string[] lines = File.ReadAllLines(sourcePath); // nõa precisa estanciar pois é estatica
+
+                foreach(string line in lines) // varrendo vetor de lines
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
