4a922d9ced7f232dd5338aca0169635802410ccc
Sh4kaa <darioo_dario@hotmail.com>
2020-06-26 17:21:44 -0300

    adicionando comentários

--- TrabalhandoComArquivos/exerfixacao/exerfixacao/Program.cs
+++ TrabalhandoComArquivos/exerfixacao/exerfixacao/Program.cs
@@ -13,24 +13,24 @@ namespace exerfixacao
         static void Main(string[] args)
         {
             Console.Write("Enter file full path: ");
-            string sourceFilePath = Console.ReadLine();
+            string sourceFilePath = Console.ReadLine(); // lendo caminho do arquivo
 
             try
             {
-                string[] lines = File.ReadAllLines(sourceFilePath);
+                string[] lines = File.ReadAllLines(sourceFilePath); // lendo o conteudo desse arquivo
 
-                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
-                string targetFolderPath = sourceFolderPath + @"\out";
-                string targetFilePath = targetFolderPath + @"\summary.csv";
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath); // obtendo as informações do caminho do arquivo
+                string targetFolderPath = sourceFolderPath + @"\out"; // atribuindo as informações do caminho do arquivo a uma variavel acrescentando mais uma pasta no caminho
+                string targetFilePath = targetFolderPath + @"\summary.csv"; // atribuindo o novo caminho gerado a uma variavel com 1 arquivo nas pasta final
 
-                Directory.CreateDirectory(targetFolderPath);
+                Directory.CreateDirectory(targetFolderPath); // criando novo diretório com base no novo caminho "out"
 
-                using (StreamWriter sw = File.AppendText(targetFilePath))
+                using (StreamWriter sw = File.AppendText(targetFilePath)) // lendo o novo arquivo criado
                 {
-                    foreach (string line in lines)
+                    foreach (string line in lines) // varrendo o conteudo do arquivo gerado
                     {
-
-                        string[] fields = line.Split(',');
+                        // removendo as virgulas do conteudo desse arquivo e atribuindo as novas posições e instanciando as informações para a classe
+                        string[] fields = line.Split(','); 
                         string name = fields[0];
                         double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
                         int quantity = int.Parse(fields[2]);
