9aed2df147f248c4c8f0e5930a4ae4d3bf48f3c0
Sh4kaa <darioo_dario@hotmail.com>
2020-06-30 15:17:50 -0300

    exerciciod e fixação (com interface)

---
+++ Orientacao_Objetos/Interfaces/exercContratoEmpresa/exercContratoEmpresa/Services/OnlinePaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace exercContratoEmpresa.Services
+{
+    interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
---
+++ Orientacao_Objetos/Interfaces/exercContratoEmpresa/exercContratoEmpresa/Services/PayPalService.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace exercContratoEmpresa.Services
+{
+    class PayPalService : IOnlinePaymentService
+    {
+        private const double FeePercentage = 0.02;
+        private const double MonthlyInterest = 0.01;
+
+        public double Interest(double amount, int months) // juros
+        {
+            return amount * MonthlyInterest * months; // quantia vezes jurosMes * quantidade de meses
+        }
+
+        public double PaymentFee(double amount) // taxa de juros
+        {
+            return amount * FeePercentage; // quantia vezes percentualTaxa
+        }
+    }
+}
