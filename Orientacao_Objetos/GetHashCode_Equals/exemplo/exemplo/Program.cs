e16ae252bb5fae1b970f163e123a4dfbd741df9f
Sh4kaa <darioo_dario@hotmail.com>
2020-07-09 14:52:34 -0300

    HashSet Equals

---
+++ Orientacao_Objetos/GetHashCode_Equals/exemplo/exemplo/Program.cs
@@ -0,0 +1,23 @@
+﻿using exemplo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b)); // true do ponto de vista de conteudo de acordo com a regra implementada que no caso é por email
+            Console.WriteLine(a == b); // vai dar falso do ponto de vista de referencia
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
---
+++ Orientacao_Objetos/GetHashCode_Equals/listHashsett/listHashsett/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listHashsett
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("Henri");
+            set.Add("Dario");
+
+            Console.WriteLine(set.Contains("Henri"));
+
+            foreach(string set1 in set)
+            {
+                Console.WriteLine(set1);
+            }
+
+        }
+    }
+}
