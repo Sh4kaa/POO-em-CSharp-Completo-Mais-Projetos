d488c08ac1689549745bdc390c3ffc1d2f731a94
Sh4kaa <darioo_dario@hotmail.com>
2020-07-20 15:42:16 -0300

    Exercicio Dictionary
    aula Dictionary e SortedDictionary + exercicio de dictionary

---
+++ Orientacao_Objetos/Dictionary/dictionaryesetDictionary/dictionaryesetDictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace dictionaryesetDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+            // cookies.Add("phone", "9868789576"); um dos jeitos de adicionar uma chave e seu valor
+            
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99771122";
+            cookies["phone"] = "99771133";
+           
+            Console.WriteLine(cookies["email"]);
+            cookies.Remove("email"); // removendo chave phone
+            Console.WriteLine("Phone number: " + cookies["phone"]);
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine("Email: " + cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is not 'email' key");
+            }
+
+            Console.WriteLine("Size: " + cookies.Count);
+            Console.WriteLine("ALL COOKIES:");
+            foreach (KeyValuePair<string, string> item in cookies) // varrendo coleção (obs: onde está o tipo de item que será percorrido no foreach
+                                                                   // pode-se colocar um "var" pra ficar menos verboso o código
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
---
+++ Orientacao_Objetos/Dictionary/exercicioResolvidoDictionary/exercicioResolvidoDictionary/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioResolvidoDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+
+
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> votacao = new Dictionary<string, int>();
+                    while (!(sr.EndOfStream))
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string candidato = line[0];
+                        int voto = int.Parse(line[1]);
+
+
+
+                        if (votacao.ContainsKey(candidato)) // passando a key "candidato"
+                        {
+                            votacao[candidato] += voto; // o dictionary "votacao" na chave "candidato" recebe o voto(tipo int)
+                        }
+                        else
+                        {
+                            votacao[candidato] = voto;
+                        }
+                    }
+
+                    foreach(KeyValuePair<string, int> votCand in votacao) // poderia usar tbem  o "var" no lugar de "KeyValuePair" para ficar menos verboso
+                    {
+                        Console.WriteLine(votCand.Key +":"+ " "+ votCand.Value );
+                    }
+                    //obs: chave recebe um apelido, no caso "canditato"(tipo string)
+
+                }
+
+
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
