0f2455e473f7224f7c5e63f707a4fe4fe42622d2
Sh4kaa <darioo_dario@hotmail.com>
2020-07-22 20:06:13 -0300

    material de apoio + extension metods

---
+++ Orientacao_Objetos/ExtensionMetods/MetodoExtendido/MetodoExtendido/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace MetodoExtendido.Extensions
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj) // metódo referenciando o próprio datetime
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+            if(duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("f1", CultureInfo.InvariantCulture) + " hours";
+            } else
+            {
+                return duration.TotalDays.ToString("f1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
---
+++ Orientacao_Objetos/ExtensionMetods/MetodoExtendido/MetodoExtendido/Program.cs
@@ -0,0 +1,17 @@
+﻿using MetodoExtendido.Extensions;
+using System;
+
+
+namespace MetodoExtendido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2020, 07, 22, 13, 10, 45);
+
+            Console.WriteLine(dt.ElapsedTime());
+
+        }
+    }
+}
