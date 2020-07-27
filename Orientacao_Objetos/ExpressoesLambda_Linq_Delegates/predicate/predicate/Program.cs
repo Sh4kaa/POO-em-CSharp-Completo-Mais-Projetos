11d236115a0f7d04428c7579ceb933fe27ef4618
Sh4kaa <darioo_dario@hotmail.com>
2020-07-27 19:24:26 -0300

    Func exemplo 4 (expressõa lambda como parâmetro)

--- Orientacao_Objetos/ExpressoesLambda_Linq_Delegates/predicate/predicate/Program.cs
+++ Orientacao_Objetos/ExpressoesLambda_Linq_Delegates/predicate/predicate/Program.cs
@@ -15,14 +15,10 @@ namespace predicate
             list.Add(new Product("Mouse", 50.00));
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
-            Func<Product, string> func = p => p.Name.ToUpper(); // expressão lâmbida
-
-            // obs: não é necessario utilizar chaves, pois em expressão lambida não é necessari quanto se trata de returonar algum valor
-            // assim como também poderia se utilizar as chaves, porém seria necessario utilizar o comando return
-            // exemplo: Func<Product, string> func = p => { return  p.Name.ToUpper();};
+            
 
 
-            List<string> result = list.Select(func).ToList(); // um segunda lista recebendo o obj func que recebeu uma referencia de uma função 
+            List<string> result = list.Select(p => p.Name.ToUpper()).ToList(); // expressão lambda como parametro
 
             foreach(string s in result)
             {
