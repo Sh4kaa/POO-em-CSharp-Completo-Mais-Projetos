15309fb93906f7be5d3c76794b7d36d3d01f4a47
Sh4kaa <darioo_dario@hotmail.com>
2020-07-27 19:21:17 -0300

    Func exemplo 3 com expressão lambda

--- Orientacao_Objetos/ExpressoesLambda_Linq_Delegates/predicate/predicate/Program.cs
+++ Orientacao_Objetos/ExpressoesLambda_Linq_Delegates/predicate/predicate/Program.cs
@@ -15,8 +15,13 @@ namespace predicate
             list.Add(new Product("Mouse", 50.00));
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
-            Func<Product, string> func = NameUpper;
-            
+            Func<Product, string> func = p => p.Name.ToUpper(); // expressão lâmbida
+
+            // obs: não é necessario utilizar chaves, pois em expressão lambida não é necessari quanto se trata de returonar algum valor
+            // assim como também poderia se utilizar as chaves, porém seria necessario utilizar o comando return
+            // exemplo: Func<Product, string> func = p => { return  p.Name.ToUpper();};
+
+
             List<string> result = list.Select(func).ToList(); // um segunda lista recebendo o obj func que recebeu uma referencia de uma função 
 
             foreach(string s in result)
@@ -24,9 +29,6 @@ namespace predicate
                 Console.WriteLine(s);
             }
         }
-        static string NameUpper(Product p)
-        {
-            return p.Name.ToUpper();
-        }
+        
     }
 }
