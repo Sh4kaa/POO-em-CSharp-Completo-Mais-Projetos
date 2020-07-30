462226b5ebd265eb47fd9b0247ed46723e7792f0
Sh4kaa <darioo_dario@hotmail.com>
2020-07-30 20:04:14 -0300

    exercicio Lista de funcionarios
    Fazer um programa para ler os dados (nome, email e salário)
    de funcionários a partir de um arquivo em formato .csv. (no caso usei o formato txt)
    Em seguida mostrar, em ordem alfabética, o email dos
    funcionários cujo salário seja superior a um dado valor
    fornecido pelo usuário.
    Mostrar também a soma dos salários dos funcionários cujo
    nome começa com a letra 'M'.
    conteudo do arquivo:
    Maria,maria@gmail.com,3200.00
    Alex,alex@gmail.com,1900.00
    Marco,marco@gmail.com,1700.00
    Bob,bob@gmail.com,3500.00
    Anna,anna@gmail.com,2800.00

--- Orientacao_Objetos/ExpressoesLambda_Linq_Delegates/exerciResolvido/exerciResolvido/Program.cs
+++ Orientacao_Objetos/ExpressoesLambda_Linq_Delegates/exerciResolvido/exerciResolvido/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.IO;
-using Course.Entities;
+using exerciResolvido.Entities;
 
 namespace exerciResolvido
 {
@@ -14,27 +14,51 @@ namespace exerciResolvido
             Console.Write("Enter full file path: ");
             string path = Console.ReadLine();
 
-            List<Product> list = new List<Product>();
-
-            using (StreamReader sr = File.OpenText(path))
+            List<Employee> list = new List<Employee>(); // lista de funcionarios
+            // bloco try
+            try
             {
-                while (!sr.EndOfStream)
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    string[] fields = sr.ReadLine().Split(',');
-                    string name = fields[0];
-                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
-                    list.Add(new Product(name, price));
+                    // lendo o arquivo de dados
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                        list.Add(new Employee(name, email, salary));
+                    }
                 }
+                
+                Console.Write("Enter salary: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine("Email of people whose salary is more than: " + amount.ToString("f2", CultureInfo.InvariantCulture));
+
+                // filtrando a lista de funcionarios por e-mail, que tenham salario maior que a quantia definida, selecionando apenas o e-mail(sem ser objeto inteiro) 
+                var funcEmail = list.Where(f => f.Salary > amount).OrderBy(f => f.Email).Select(f => f.Email); 
+
+                foreach (string email in funcEmail)
+                {
+                    Console.WriteLine(email);
+                }
+
+                double sum = list.Where(f => f.Name[0] == 'M').Sum(f => f.Salary);
+                Console.Write("Sum of salary of people whose name starts with 'M': " + sum.ToString("f2", CultureInfo.InvariantCulture));
+                Console.WriteLine();
             }
 
-            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average(); 
-            Console.WriteLine("Average price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
-
-            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name); // select serve apenas para pegar apenas o nome e não o objeto inteiro
-            foreach (string name in names)
+            // bloco catch
+            catch(IOException e)
             {
-                Console.WriteLine(name);
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
             }
+
+
+
+
         }
     }
 }
